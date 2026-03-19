2eaaf85c56193854424c7314f0ad4a985443b36e
dariusss23 <savadarius23@gmail.com>
2026-03-19 18:50:32 +0200

    Add files via upload

---
+++ PROIECT.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+
+ifstream fin("EVOLUTIE.IN");
+ofstream fout("EVOLUTIE.out");
+
+int n, a, b, coef_A, coef_B, coef_C, prob_recombinare, prob_mutatie, etape, precizie;
+
+int nr_biti;
+double pas_discretizare;
+double probabilitatea[1005], capat_selectie[1005];
+
+struct Individ{
+    string cromozom;
+    double x;
+    double fitness;
+};
+
+
+Individ populatie[1005];
+
+void citire(){
+    cout<<"Dimensiunea populatiei: ";
+    fin>>n;
+    cout<<"Coeficientul 'a' al ecuatiei a * x^2 + b * x + c : ";
+    fin>>coef_A;
+    cout<<"Coeficientul 'b' al ecuatiei "<<coef_A<<" * x^2 + b * x + c : ";
+    fin>>coef_B;
+    cout<<"Coeficientul 'c' al ecuatiei "<<coef_A<<" x^2 + "<<coef_B<<" * x + c : ";
+    fin>>coef_C;
+    cout<<"Functia : "<<coef_A<<" * x^2 + "<<coef_B<<" * x + "<<coef_C<<'\n';
+    cout<<"Precizie : ";
+    fin>>precizie;
+    cout<<"Domeniul capat stanga [ _ , ] : ";
+    fin>>a;
+    cout<<"Domeniul capat dreapta:["<<a<<" , _ ] : ";
+    fin>>b;
+    cout<<"Domeniul : [ "<<a<<" , "<<b<<" ]"<<'\n';
+    cout<<"Probabilitatea de recombinare (? %) : ";
+    fin>>prob_recombinare;
+    cout<<"Probabilitatea de  mutatie (? %) : ";
+    fin>>prob_mutatie;
+    cout<<"Numar etape : ";
+    fin>>etape;
+}
+
+string random_bits(){
+    string s(nr_biti, '0');
+    for (char &c : s){
+        c = '0' + rand() % 2;
+    }
+    return s;
+}
+
+double decode(string cromozom){
+    long long k = 0;
+    for (char c : cromozom)
+        k = k * 2 + (c - '0');
+    return a + k * pas_discretizare;
+}
+
+double fitness(double x){
+    return coef_A * x * x + coef_B * x + coef_C;
+}
+
+void afiseaza_populatie_initiala() {
+    fout<<"Populatia initiala"<<'\n';
+    for (int i = 1; i <= n; i++) {
+        fout<<setw(3)<<i<<" : "<<populatie[i].cromozom<<"  x = "<<setw(10)<<populatie[i].x<<"  f = "<<setw(10)<<populatie[i].fitness<<'\n';
+    }
+}
+
+void selectie(){
+    double sum = 0;
+    for (int i=1; i<=n; i++)
+        sum += populatie[i].fitness;
+
+    fout<<'\n';
+    fout<<"Probabilitati selectie"<<'\n';
+    for (int i=1; i<=n; i++){
+        probabilitatea[i] = populatie[i].fitness / sum;
+
+        fout<<setw(11)<<"cromozom " <<setw(4)<<i<<" probabilitate "<<setw(12)<<probabilitatea[i]<<'\n';
+    }
+}
+
+void intervale_selectie(){
+    for (int i=1; i<=n; i++){
+        capat_selectie[i] = i==1 ? 0 : capat_selectie[i-1] + probabilitatea[i];
+    }
+
+    fout<<'\n';
+    fout<<"Intervale probabilitati selectie"<<'\n';
+    for (int i=1; i<n; i++){
+        fout<<"[ "<<capat_selectie[i]<<" , "<<capat_selectie[i+1]<<" ]"<<'\n';
+    }
+    fout<<"[ "<<capat_selectie[n]<<" , "<<"1.0"<<" ]"<<'\n';
+}
+
+int cauta_interval(double u) {
+    int stanga = 1, dreapta = n;
+    while (stanga < dreapta) {
+        int mid = (stanga + dreapta) / 2;
+        if (u < capat_selectie[mid+1]) dreapta = mid;
+        else stanga = mid + 1;
+    }
+    return stanga;
+}
+
+void selecteaza_populatie_noua() {
+    Individ populatie_noua[1005];
+    
+    int elite=1;
+    for (int i=2; i<=n; i++)
+        if (populatie[i].fitness > populatie[elite].fitness) 
+            elite=i;
+    
+    fout<<'\n';
+    for (int i=1; i<=n; i++){
+        double random = (double)rand() / RAND_MAX;
+        int index_cromozom = cauta_interval(random);
+
+        fout<<"u = "<<setprecision(16)<<random<< "  ->  selectam cromozomul "<<index_cromozom<<'\n';
+        populatie_noua[i] = populatie[index_cromozom];
+    }
+    populatie_noua[1] = populatie[elite];
+
+    fout<<'\n';
+    fout<<"Dupa selectie:\n";
+    for (int i = 1; i <= n; i++) {
+        populatie[i] = populatie_noua[i];
+        fout<<setw(3)<<i<<" : "<<populatie[i].cromozom<<"  x = "<<setw(10)<<setprecision(6)<<populatie[i].x<<"  f = " <<setw(10)<<setprecision(6)<<populatie[i].fitness<<'\n';
+    }
+}
+
+void crossover(){
+    double prob_cross = (double)prob_recombinare/100;
+    fout<<'\n';
+    fout<<"Probabilitatea de incrucisare "<<prob_cross<<'\n';
+
+    int participare[1005], k=0;
+    fout<<'\n';
+    for (int i=1; i<=n; i++){
+        double random = (double)rand() / RAND_MAX;
+
+        fout<<setw(3)<<i<<": " <<populatie[i].cromozom<< "  u = " <<setprecision(16)<<random;
+        if (random < prob_cross) fout<<" < "<<prob_cross<<"  participa";
+        fout<<'\n';
+
+        if (random < prob_cross) participare[++k] = i;
+
+    }
+
+    for (int j=1; j+1<=k; j+=2){
+        int x = participare[j];
+        int y = participare[j+1];
+
+        int punct_rupere = 1 + rand() % (nr_biti - 1);
+
+        fout<<"\nRecombinare intre cromozomul "<<x<< " si cromozomul "<<y<<":\n";
+        fout<<populatie[x].cromozom<<"  "<<populatie[y].cromozom<<"  punct = "<<punct_rupere<<'\n';
+
+        string newX = populatie[x].cromozom.substr(0, punct_rupere) + populatie[y].cromozom.substr(punct_rupere);
+        string newY = populatie[y].cromozom.substr(0, punct_rupere) + populatie[x].cromozom.substr(punct_rupere);
+
+        populatie[x].cromozom = newX;
+        populatie[x].x = decode(newX);
+        populatie[x].fitness = fitness(populatie[x].x);
+
+        populatie[y].cromozom = newY;
+        populatie[y].x = decode(newY);
+        populatie[y].fitness = fitness(populatie[y].x);
+
+        fout<<"Rezultat:  "<<populatie[x].cromozom<<"  "<<populatie[y].cromozom<<'\n';
+    }
+}
+
+int main(){
+    citire();
+
+    nr_biti = ceil(log2((b - a) * pow(10, precizie)));
+    pas_discretizare = (b-a)/pow(2,nr_biti);
+
+    for (int i=1; i<=n; i++){
+        populatie[i].cromozom =  random_bits();
+        populatie[i].x = decode(populatie[i].cromozom);
+        populatie[i].fitness = fitness(populatie[i].x);
+    }
+
+    afiseaza_populatie_initiala();
+
+    selectie();
+
+    intervale_selectie();
+
+    selecteaza_populatie_noua();
+
+    crossover();
+}
